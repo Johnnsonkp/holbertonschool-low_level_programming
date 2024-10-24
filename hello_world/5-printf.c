592b6a4e6c67ad138831f4d6b45898a13ecabdcb
johnnsonkp <john.nkp1@gmail.com>
2024-10-24 09:03:11 +0000

    hello print challenge

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function prints a message to the console using the printf function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
