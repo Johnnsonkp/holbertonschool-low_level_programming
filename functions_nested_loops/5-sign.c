738e7a8917e264dd779cc9698eeb77139a65a295
“johnnsonkp” <john.nkp1@gmail.com>
2024-10-27 18:29:16 +0000

    print number and sign challenge

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - prints the sign of a number
+ * @n: the int being compared to 0
+ *
+ * Return: returns 1 || -1 || 0 if n is > than, < than or == to 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		printf('-')
+		return (-1);
+	}
+	printf('0');
+	return (0);
+}
