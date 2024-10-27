db9aa58e2965ab427a354866929ed5f624a1de80
“johnnsonkp” <john.nkp1@gmail.com>
2024-10-27 16:19:21 +0000

    print alpabet x10 challenge

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c < 'z'; i++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
