71a83cb9e81773f97d1538df04aedba818e2f926
“johnnsonkp” <john.nkp1@gmail.com>
2024-10-27 17:24:55 +0000

    is lower function

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - checks if the input is a lowercase character
+ * @c: checked input
+ *
+ * Return: Returns 1 if lowercase, otherwise 0
+ */
+int _islower(int c)
+{
+	if (islower)
+	{
+		return (1);
+	}
+	return (0);
+}
