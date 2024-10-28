58c4a0bea8d1dbb34747522b1da4be5090014a07
Johnnsonkp <john.nkp1@gmail.com>
2024-10-28 12:11:20 +0000

    times table challenge

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,11 +12,11 @@ void times_table(void)
 
 	while (multiplier <= 9)
 	{
-		for (num = 0; num <= 9; num++)
+		for (num = 1; num <= 9; num++)
 		{
 			int multiple = num * multiplier;
 
-			if (num == 0)
+			if (multiplier == 0)
 			{
 				_putchar('0');
 			}
