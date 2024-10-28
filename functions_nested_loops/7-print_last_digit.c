d994cc1a35d5d62029aaaabda169fa54d0ff0adb
“johnnsonkp” <john.nkp1@gmail.com>
2024-10-28 05:16:00 +0000

    print last digit challenge

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @num: The number we're taking the last digit of
+ * Return: Always 0 (success)
+ */
+int print_last_digit(int num)
+{
+	int lastDigit;
+
+	if (num)
+	{
+		lastDigit = num % 10
+		return (lastDigit);
+	}
+}
