d745264e9c4eca71b3502d9e949e507e2a311808
Johnnsonkp <john.nkp1@gmail.com>
2024-10-29 02:24:01 +0000

    two int challenge

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two integers and returns the result
+ * @num_1: the first number to be added
+ * @num_2: the second number to be added
+ * Return: Always 0 (success)
+ */
+int add(int num_1, int num_2)
+{
+	return (num_1 + num_2);
+}
