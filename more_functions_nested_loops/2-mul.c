5797ab8eda636aa0cac113bdeda447fed9d91a64
Johnnsonkp <john.nkp1@gmail.com>
2024-10-30 16:30:31 +0000

    initial commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Multiplies multiple integers
+ * @a: first varable to be multiplied
+ * @b: second variable to be multiplied
+ * Return: the multiplied value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
