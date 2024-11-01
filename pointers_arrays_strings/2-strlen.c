6050f604a24dd486bb32140fb0ecd7d525f8a2f2
Johnnsonkp <john.nkp1@gmail.com>
2024-11-01 15:34:26 +0000

    lenght of string challenge

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string
+ * @s: variable holding the string
+ *
+ * Return: Always 0 (success)
+ */
+int _strlen(char *s)
+{
+	size_t length = strlen(s);
+
+	return (length);
+}
