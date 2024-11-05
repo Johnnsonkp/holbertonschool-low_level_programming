6f04a7986b589a1b8e4e503b587262820377722c
Johnnsonkp <john.nkp1@gmail.com>
2024-11-05 21:27:28 +0000

    initial commit

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,16 +12,14 @@ char *_strcat(char *dest, char *src)
 	int index = 0;
 	int i = 0;
 
-	while (dest[index])
-	{	index++;
-		if (dest[index] == '\0')
-		{
-			while (src[i])
-			{
-				dest[index + i] = src[i];
-				i++;
-			}
-		}
+	while (dest[index] != '\0')
+	{
+		index++;
+	}
+	while (src[i] != '\0')
+	{
+		dest[index + i] = src[i];
+		i++;
 	}
 	dest[index + i + 1] = '\0';
 	return (dest);
