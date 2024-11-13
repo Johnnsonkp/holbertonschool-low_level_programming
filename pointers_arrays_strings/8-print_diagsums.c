88bef8ad7c99619deb2bfff029d7e0cfa7f0e31e
Johnnsonkp <john.nkp1@gmail.com>
2024-11-13 05:54:18 +0000

    7- completed Diagsums challenge with:   126  vi main.h   127  vi 8-print_diagsums.c   128  vi 008-main.c

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square
+ * @a: pointer
+ * @size: size
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int j = 0;
+	int t = 0;
+	int s = size * size;
+
+	for (i = 0; i < s; i += size + 1)
+	{
+		j += a[i];
+	}
+
+	for (i = size - 1; i < s - 1; i += size - 1)
+	{
+		t += a[i];
+	}
+
+	printf("%d, %d\n", j, t);
+}
