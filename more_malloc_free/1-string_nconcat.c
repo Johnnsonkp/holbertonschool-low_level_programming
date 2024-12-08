4b359347409bc650f5e641b561884422b75eefd1
Johnnsonkp <john.nkp1@gmail.com>
2024-12-08 04:57:47 +0000

    initial commit

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+#include "main.h"
+/**
+ * string_nconcat - function that concatenates two strings
+ * @s1: string 1 
+ * @s2: string to concate to @s1
+ * @n: number of char from @s2 to concat to @s1
+ * Return: pointer point to a newly allocated space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	return (NULL);
+}
