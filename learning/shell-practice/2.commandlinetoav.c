68feff45540bd26cea94c5556b6c8d7dfefafb0f
Johnnsonkp <john.nkp1@gmail.com>
2024-12-10 17:15:26 +0000

    finished shell practice 2.commandlinetoav. challenge with: gcc 2.commandlinetoav.c o- 2.commandlinetoav   138  vi 2.commandlinetoav.c   139  gcc 2.commandlinetoav.c o- 2.commandlinetoav   140  vi 2.commandlinetoav.c   141  gcc 2.commandlinetoav.c o- 2.commandlinetoav   142  vi 2.commandlinetoav.c   143  gcc 2.commandlinetoav.c o- 2.commandlinetoav   144  vi 2.commandlinetoav.c   145  gcc 2.commandlinetoav.c o- 2.commandlinetoav   146  gcc 2.commandlinetoav.c o- -E 2.commandlinetoav   147  gcc 2.commandlinetoav.c -E 2.commandlinetoav   148  gcc 2.commandlinetoav.c o- 2.commandlinetoav   149  gcc 2.commandlinetoav.c -o 2.commandlinetoav   150  ls   151  ./2.commandlinetoav testing yes///yes----noo   152  vi 2.commandlinetoav.c   153  gcc 2.commandlinetoav.c -o 2.commandlinetoav   154  ./2.commandlinetoav testing yes///yes----noo   155  ./2.commandlinetoav   156  ./2.commandlinetoav test--test   157  ./2.commandlinetoav "hello/world:this;is=a-test"   158  PROMPT_DIRTRIM=1   159  ./2.commandlinetoav "hello/world:this;is=a-test"   160  PROMPT_DIRTRIM=1   161  ./2.commandlinetoav "hello/world:this;is=a-test"   162  PROMPT_DIRTRIM=1   163  vi 2.commandlinetoav.c   164  gcc 2.commandlinetoav.c -o 2.commandlinetoav   165  ./2.commandlinetoav "hello/world:this;is=a-test"

--- learning/shell-practice/2.commandlinetoav.c
+++ learning/shell-practice/2.commandlinetoav.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - splits & returns an array of each word of a string
  * Return: (0) if successfl
  */
-int main(void)
+int main(int ac, char **av)
 {
-	
+	char *sep = "\\/:;=-";
+	char *token;
+	char *input = av[1];
+
+	token = strtok(input, sep);
+
+	while (token != NULL)
+	{
+		printf("%s\n", token);
+		token = strtok(NULL, sep);
+	}
+
+	return (0);
 }
